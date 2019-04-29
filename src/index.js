7b9a4cc38ee78256135e82deab60b720a390fb57
Ashley King <ashleyot@gmail.com>
2019-04-29 16:50:17 -0400

    outlined envelope and hard coded components. TODO: make them dynamic, add proptypes declarations, finish crappy css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Envelope(){
+    return(
+        <div className="envelope">
+         <Stamp/>
+        <ReturnAddress/>
+        <SenderAddress/>
+    </div>
+    )
+    
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function ReturnAddress(){
+    return(
+        <div className="return-address-container">
+             <div className="name">
+            Ashley King
+        </div>    
+        <div className="address">
+            16 Ohana Lane
+        </div>
+        <div className="city-state">
+            Kauai, HI 96714
+        </div>
+        </div>
+       
+    )
+}
+
+function SenderAddress(){
+    return(
+        <div className="sender-address-container">
+            <div className="name">
+                To Person
+            </div>
+            <div className="address">
+                70 To Address St.
+            </div>
+            <div className="city-state">
+                Kauai, HI, 96715
+            </div>
+        </div>
+    )
+}
+
+const Stamp = () => (
+    <div className="stamp"></div>
+)
+    
+
+
+
+ReactDOM.render(<Envelope />, document.getElementById('root'));
+
